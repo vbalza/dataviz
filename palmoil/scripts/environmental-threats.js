4308441fa0fd37efd36fed1349ff2c7eae5ce416
vbalza <79028704+vbalza@users.noreply.github.com>
2022-04-06 09:01:15 -0500

    Update environmental-threats.js

--- palmoil/scripts/environmental-threats.js
+++ palmoil/scripts/environmental-threats.js
@@ -13,14 +13,17 @@ d3.csv("data/deforestation.csv").then(function(data) {
         .attr("height", height + margin.top + margin.bottom)
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    let x = d3.scaleLinear()
-        .domain(d3.extent(data, function(d) { return d.year; }))
-        .range([ 0, width ]);
+    const subgroups = data.columns.slice(1)
+    const groups = data.map(d => (d.year))
+
+    let x = d3.scaleBand()
+        .domain(groups)
+        .range([ 0, width ])
+        .padding([0.2]);
     
     let xAxisSettings = d3.axisBottom(x)
-        .tickValues([2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015])
         .tickFormat(d3.format("d"))
-        .tickPadding(10);
+        .tickSizeOuter(0)
     
     let xAxis = svgThreats.append("g")
         .attr("class", "x axis")
@@ -50,17 +53,28 @@ d3.csv("data/deforestation.csv").then(function(data) {
     svgThreats.append("g")
         .call(yAxisSettings)
   
-    let keys = data.columns.slice(1)
-  
     let color = d3.scaleOrdinal()
-        .domain(keys)
+        .domain(subgroups)
         .range(["#bad0af", 
                 "#488f31" ]);
   
     let stackedData = d3.stack()
-        .keys(keys)
+        .keys(subgroups)
         (data)
-
+    
+    let barChart = svgThreats.append("g")
+        .selectAll("g")
+        .data(stackedData)
+        .join("g")
+        .attr("fill", d => color(d.key))
+        .selectAll("rect")
+        .data(d => d)
+        .join("rect")
+        .attr("x", d => x(d.data.year))
+        .attr("y", d => y(d[1]))
+        .attr("height", d => y(d[0]) - y(d[1]))
+        .attr("width",x.bandwidth())
+    
     svgThreats.append("line")
         .attr("x1", margin)
         .attr("x2", width)
@@ -68,97 +82,5 @@ d3.csv("data/deforestation.csv").then(function(data) {
         .attr("y2", y(0))
         .style("stroke", "black")
         .style("stroke-width", "1.5px")
-
-    let clip = svgThreats.append("defs").append("svg:clipPath")
-        .attr("id", "clip")
-        .append("svg:rect")
-        .attr("width", width )
-        .attr("height", height )
-        .attr("x", 0)
-        .attr("y", 0);
-
-    let brush = d3.brushX()                 
-        .extent( [ [0,0], [width,height] ] ) 
-        .on("end", updateChart)
-
-    let areaChart = svgThreats.append('g')
-        .attr("clip-path", "url(#clip)")
-
-    let area = d3.area()
-        .x(function(d) { return x(d.data.year); })
-        .y0(function(d) { return y(d[0]); })
-        .y1(function(d) { return y(d[1]); });
-
-    areaChart
-        .selectAll("mylayers")
-        .data(stackedData)
-        .join("path")
-        .attr("class", function(d) { return "myArea" + d.key })
-        .style("fill", function(d) { return color(d.key); })
-        .attr("d", area)
-
-    areaChart
-        .append("g")
-        .attr("class", "brush")
-        .call(brush);
-
-    let idleTimeout
-    function idled() { idleTimeout = null; }
-
-    function updateChart(event,d) {
-        extent = event.selection
-
-        if(!extent){
-            if (!idleTimeout) 
-            return idleTimeout = setTimeout(idled, 350);
-            x.domain(d3.extent(data, function(d) { return d.year; }))
-        } else{
-            x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
-            areaChart.select(".brush").call(brush.move, null)
-        }
-
-      xAxis.transition()
-          .duration(1000)
-          .call(d3.axisBottom(x).ticks(5).tickFormat(d3.format("d")))
-
-      areaChart
-          .selectAll("path")
-          .transition().duration(1000)
-          .attr("d", area)
-    }
-
-    let highlight = function(event, d){
-        d3.selectAll(".myArea").style("opacity", .1)
-        d3.select("."+d).style("opacity", 1)
-    }
-
-    let noHighlight = function(event,d){
-        d3.selectAll(".myArea").style("opacity", 1)
-    }
-
-    let size = 20
-    
-    svgThreats.selectAll("myrect")
-        .data(keys)
-        .join("rect")
-        .attr("x", 600)
-        .attr("y", function(d, i){ return 10 + i*(size+5)})
-        .attr("width", size)
-        .attr("height", size)
-        .style("fill", function(d){ return color(d) })
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
-
-    svgThreats.selectAll("mylabels")
-        .data(keys)
-        .join("text")
-        .attr("x", 600 + size*1.2)
-        .attr("y", function(d, i){ return 10 + i*(size+5) + (size/2)})
-        .style("fill", "black")
-        .text(function(d){ return d})
-        .attr("text-anchor", "left")
-        .style("alignment-baseline", "middle")
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
 
 })
